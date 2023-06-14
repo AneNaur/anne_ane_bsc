852016f6c9e94b082bc24fecd2d72ada3b9092b0
Ane <112382000+AneNaur@users.noreply.github.com>
2023-06-14 16:16:21 +0200

    Update codeml2.py

--- codeml2.py
+++ codeml2.py
@@ -27,7 +27,7 @@ cml = codeml.Codeml(alignment = args.alignment_file, tree = args.tree_file,
 
 
 cml.set_options(noisy = 0, verbose=1, seqtype=1, ndata = 1, icode = 0, cleandata = 1, NSsites=[ 1, 2, 7, 8], CodonFreq = 2, fix_omega = 0, omega = 1, clock = 0, fix_alpha = 1, alpha = 0, getSE = 0)
-#cml.set_options(runmode = 0, model = 0, aaDist = 0, Mgene = 0, fix_kappa = 0, kappa = 2, Malpha = 0, ncatG = 1, RateAncestor = 0, Small_Diff = .5e-06, method = 0, fix_blength = None)
+cml.set_options(runmode = 0, model = 0, aaDist = 0, Mgene = 0, fix_kappa = 0, kappa = 2, Malpha = 0, ncatG = 1, RateAncestor = 0, Small_Diff = .5e-06, method = 0, fix_blength = None)
 cml.ctl_file = args.control_file
 
 
